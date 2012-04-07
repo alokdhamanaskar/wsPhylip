078f0ab9d56b8ffdcfbd7e02d312836a4768fd26
Alok Dhamanaskar <alokdhamanaskar@gmail.com>
2012-04-07 19:04:45 -0400

    resolved

--- tmp/PhylipConsense:09dab6e5-c0f6-4b65-a2e0-a6023fa46277/consense.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd tmp/PhylipConsense:09dab6e5-c0f6-4b65-a2e0-a6023fa46277
-phylip consense <<EOD
-query.txt
-C
-C
-R
-Y
-EOD
--- tmp/PhylipConsense:2c11e1af-403a-49e2-89a9-a40ce6f056ed/consense.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd tmp/PhylipConsense:2c11e1af-403a-49e2-89a9-a40ce6f056ed
-phylip consense <<EOD
-query.txt
-C
-C
-R
-Y
-EOD
